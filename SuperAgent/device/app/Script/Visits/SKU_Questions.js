58e7a0f2d53b1dd5175fa8a8d4cd8d2ec3b068ab
AVMurach <AVMurach@1cbit.ru>
2015-06-27 22:23:59 +0300

    fix https://bitmobile.atlassian.net/browse/INT-118 402.5.1.113

--- SuperAgent/device/app/Script/Visits/SKU_Questions.js
+++ SuperAgent/device/app/Script/Visits/SKU_Questions.js
@@ -199,21 +199,12 @@ function GetSKUsFromQuesionnaires_NTZ_m(search) {
 			// ", CASE WHEN S.SKU=@currentSKU THEN 1 ELSE 0 END AS ShowChild " +
 			", CASE WHEN S.SKU=@currentSKU THEN 1 ELSE 1 END AS ShowChild " + 
 			
-			", Matrix_SKU.Qty AS MatrixQty "
-			+
+			
 
 			"FROM USR_SKUQuestions S "
 			+ filterJoin
 			
-			+ "LEFT JOIN (SELECT Matrix_SKU.SKU, MAX(Matrix_SKU.Qty) AS Qty	" +
-				"FROM Catalog_AssortmentMatrix_SKUs Matrix_SKU " +
-				"WHERE Matrix_SKU.Ref IN (SELECT DISTINCT Ref " +
-				"FROM Catalog_AssortmentMatrix_Outlets " +
-				"WHERE Outlet = @outlet) " +
-				"GROUP BY Matrix_SKU.SKU) Matrix_SKU ON Matrix_SKU.SKU = S.SKU " +
-			
-			
-			"WHERE Single=@single AND "
+			+ "WHERE Single=@single AND "
 			+ searchString
 			+ filterString
 			+ " (ParentQuestion=@emptyRef OR ParentQuestion IN (SELECT Question FROM USR_SKUQuestions SS "
